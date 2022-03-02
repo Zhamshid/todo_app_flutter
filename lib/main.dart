82f981358728e7babb694bbd5a69105d0a49a5ae
Zhamshid Irisbayev <180103263@stu.sdu.edu.kz>
2022-03-02 15:40:10 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const TodoApp());
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      appBar: AppBar(
+        title: Text('To Do App'),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+      ),
+
+    );
+  }
+}
+
+
+
